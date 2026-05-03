ef2ff07e7e85cce4cfe0ff04b74d3e6b482fe66f
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-03 14:19:26 +0700

    feat(user): new user sqlmodel for users table

---
+++ src/infra/models/user.py
@@ -0,0 +1,15 @@
+from uuid import UUID, uuid4
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import String
+
+from src.shared.base_model import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
+    password_hash: Mapped[str] = mapped_column(String(120))
