01666139a54b0a25b0690045fb10e53c3fd3131e
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-16 21:01:18 +0700

    remove(user_repo): redundant method for user repo

--- src/infra/repos/user_repo.py
+++ src/infra/repos/user_repo.py
@@ -9,6 +9,3 @@ class UserRepo(BaseRepo[User]):
 
     def __init__(self, session: Session) -> None:
         super().__init__(session)
-
-    def get_by_username(self, username: str) -> User | None:
-        return self.get_by(username=username)
