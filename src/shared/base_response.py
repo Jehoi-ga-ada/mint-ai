6b068033700716060828aa85706ba34d80e3b98d
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-17 17:53:32 +0700

    refactor(response): user raise http exception instead of APIResponse

--- src/shared/base_response.py
+++ src/shared/base_response.py
@@ -32,15 +32,3 @@ class APIResponse(BaseModel, Generic[T]):
             status="Error",
             message=message,
         )
-
-    @classmethod
-    def unauthorized(
-        cls,
-        message: str = "Could not validate credentials",
-        code: int = status.HTTP_401_UNAUTHORIZED,
-    ):
-        return cls(
-            code=code,
-            status="Unauthorized",
-            message=message,
-        )
