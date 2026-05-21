5810ae9a7802b07aac5978ba599f47ce6ef536db
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-21 21:21:37 +0700

    refactor(generation): now use a shared chat object

--- src/graph/chains/generation.py
+++ src/graph/chains/generation.py
@@ -1,9 +1,9 @@
 from langchain_core.output_parsers import StrOutputParser
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langsmith import Client
 
+from src.graph.chains.llm import llm
+
 client = Client()
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 
 prompt_template = client.pull_prompt(
     "rlm/rag-prompt", dangerously_pull_public_prompt=True
