042fc0f4470d9929abde2c909ea261badf34ed06
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-22 16:52:45 +0700

    feat(config): config for langsmith and tavily

--- src/infra/config.py
+++ src/infra/config.py
@@ -9,5 +9,14 @@ class Config(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
+    langsmith_tracing: bool
+    langsmith_endpoint: str
+    langsmith_api_key: str
+    langsmith_project: str
+
+    gemini_api_key: str
+
+    tavily_api_key: str
+
 
 config = Config()
