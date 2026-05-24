b0baedc86a2b03e5a846b855991e54f62f0257b6
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-24 15:23:33 +0700

    refactor(graph): remove exporting mermaid png

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -1,3 +1,4 @@
+from langchain_core.messages import HumanMessage
 from langgraph.graph.state import START, StateGraph
 from langgraph.prebuilt import tools_condition
 
@@ -14,5 +15,3 @@ workflow.add_edge(START, REASONING)
 workflow.add_conditional_edges(REASONING, tools_condition)
 workflow.add_edge(TOOLS, REASONING)
 graph = workflow.compile()
-
-graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
