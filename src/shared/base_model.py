be34f678031a98059b46a6f081d1b01c7ef7aa58
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-05-27 21:19:14 +0700

    feat(base): updated_at now defaults to now

--- src/shared/base_model.py
+++ src/shared/base_model.py
@@ -6,4 +6,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(default=func.now())
-    updated_at: Mapped[datetime | None] = mapped_column(server_onupdate=func.now())
+    updated_at: Mapped[datetime | None] = mapped_column(
+        server_onupdate=func.now(),
+        server_default=func.now(),
+    )
